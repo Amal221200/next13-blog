b78f7c526be07ed7c2418a5a826c075ee5f61794
Amal221200 <amalmurikkoli2000@gmail.com>
2023-09-08 12:31:00 +0530

    Featured component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,23 @@
+import Image from "next/image";
 import styles from "./featured.module.css";
 
 const Featured = () => {
   return (
-    <header className={styles.container}>
-      Featured
-    </header>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Amal Murikkoli here!</b> Discover my cover stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" fill alt="post1" className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, iste?</h1>
+          <p className={styles.postDesc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia quo quam deleniti quaerat, quos sunt. Nulla, odio quos maiores odit commodi ducimus voluptatem voluptas, dolor incidunt laboriosam, corrupti suscipit fugit.</p>
+          <button className={styles.button} type="button">Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Featured
